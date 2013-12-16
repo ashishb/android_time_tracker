12b27400659c6934c6518d80ad389af490211213
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2013-12-16 15:15:16 -0800

    Improved the code, added persistant storage.

--- src/net/ashishb/android_time_tracker/Main.java
+++ src/net/ashishb/android_time_tracker/Main.java
@@ -11,38 +11,51 @@ import android.content.pm.PackageManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.Vector;
 
 public class Main extends ListActivity
 {
-		public static final String TAG = "AndroidTimeTracker";
-		ArrayAdapter<String> listAdapter;
-		ListView listView;
+	public static final String TAG = "AndroidTimeTracker";
+	ArrayAdapter<String> listAdapter;
+	ListView listView;
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState){
+	/** Called when the activity is first created. */
+	@Override
+		public void onCreate(Bundle savedInstanceState){
 			super.onCreate(savedInstanceState);
 			Context context = getApplicationContext();
 			context.startService(new Intent(context, TimeTrackingService.class));
 		}
 
-		@Override
+	@Override
 		public void onResume() {
 			super.onResume();
 			listView = getListView();
 			listView.setTextFilterEnabled(true);
-			HashMap<String, Integer> values = 
-				TimeTrackingService.getPackageUseCount();
+			Set< Entry<String, Integer> > values = 
+				TimeTrackingService.getPackageUseCount().entrySet();
+			TreeSet< Entry<String, Integer> > sortedSet = new TreeSet(new Comparator<Entry<String, Integer>>() {
+				@Override
+				public int compare(Entry<String, Integer> e1, Entry<String, Integer> e2) {
+					return (-1) * (e1.getValue() - e2.getValue());
+				}
+			});
+			sortedSet.addAll(values);
 			Vector<String> entries = new Vector<String>();
-			for (Entry<String, Integer> entry : values.entrySet()) {
+			Iterator<Entry<String, Integer>> iterator = sortedSet.iterator();
+			while (iterator.hasNext()) {
+				Entry<String, Integer> entry = iterator.next();
 				entries.add(entry.getKey() + ": " + entry.getValue());
 			}
 			listAdapter = new ArrayAdapter(this,
 					android.R.layout.simple_list_item_1, entries.toArray());
 			listView.setAdapter(listAdapter);
-    }
+		}
 }
--- src/net/ashishb/android_time_tracker/TimeTrackingService.java
+++ src/net/ashishb/android_time_tracker/TimeTrackingService.java
@@ -5,19 +5,27 @@ import android.app.ActivityManager;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class TimeTrackingService extends IntentService {
 
 	public static final String TAG = "AndroidTimeTracker";
   // Change it to 10 seconds for the release.
 	public static final int SLEEP_TIME_IN_MILLISECONDS = 1000;
+	// Dump to persistant store after this recordings of pkg use count.
+	public static final int PERSISTANT_DUMP_INTERVAL_IN_RECORDING_UNITS = 256;
 	// Map of Package name to count of seconds for which app has been used.
 	private static HashMap<String, Integer> pkgUseCount = new HashMap<String, Integer>(100);
+
+	SharedPreferences pkgCountPersistantStore = null;
 
 	public TimeTrackingService() {
 		super("TimeTrackingService");
@@ -26,7 +34,22 @@ public class TimeTrackingService extends IntentService {
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		Log.i(TAG, "Service started.");
+		// One time init of pkgUseCount.
+		if (pkgCountPersistantStore == null) {
+			Log.i(TAG, "Reading from the persistant store.");
+			pkgCountPersistantStore = PreferenceManager.getDefaultSharedPreferences(this);
+			Map<String, ?> pkgCountMap = pkgCountPersistantStore.getAll();
+			for ( Entry<String, ?> entry: pkgCountMap.entrySet()) {
+				String pkgName = entry.getKey();
+				int count = (Integer)entry.getValue();
+				pkgUseCount.put(pkgName, count);
+				Log.d(TAG, String.format("pkgName: %s count: %d", pkgName, count));
+			}
+		}
+
+		int dump_count = 0;
 		while(true) {
+			dump_count++;
 			// TODO: also, check that the screen is ON.
 			String value = getCurrentlyUsedPackage();
 			Integer currentUse = pkgUseCount.get(value);
@@ -37,6 +60,10 @@ public class TimeTrackingService extends IntentService {
 			}
 			Log.i(TAG, "" + value);
 			SystemClock.sleep(1000);
+
+			if ( (dump_count & PERSISTANT_DUMP_INTERVAL_IN_RECORDING_UNITS) == 0) {
+				dumpToPersistantStore();
+			}
 		}
 	}
 
@@ -51,5 +78,16 @@ public class TimeTrackingService extends IntentService {
 		 	mActivityManager.getRunningTasks(1);
 		ActivityManager.RunningTaskInfo ar = RunningTask.get(0);
 		return ar.topActivity.getPackageName().toString();
+	}
+
+	public void dumpToPersistantStore() {
+		Log.d(TAG, "Dumping to persistant storage.");
+		SharedPreferences.Editor editor = pkgCountPersistantStore.edit();
+		if (pkgUseCount != null) {
+			for (Entry<String, Integer> entry: pkgUseCount.entrySet()) {
+				editor.putInt(entry.getKey(), entry.getValue());
+			}
+		}
+		editor.commit();
 	}
 }
